84faa32f9f62110e39fe467b7768907789d36fb1
Your Name <you@example.com>
2022-08-06 00:12:38 -0300

    first commit

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'meu-primeiro-component',
+    template: '<p>Meu Primeiro Component com angular!</p>'
+})
+export class MeuPrimeiroComponent {}
